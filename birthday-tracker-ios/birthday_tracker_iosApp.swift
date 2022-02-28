0da193cc0a3c9c7912714bcd207b5ac5ee3def0f
idlexar <idlexar@gmail.com>
2022-02-28 14:46:34 +0300

    Initial Commit

---
+++ birthday-tracker-ios/birthday_tracker_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  birthday_tracker_iosApp.swift
+//  birthday-tracker-ios
+//
+//  Created by Darvin on 28.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct birthday_tracker_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
