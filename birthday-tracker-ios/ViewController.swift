fcc8fafb5605be7ce2f9a379a74c4fd9b53611e4
idlexar <idlexar@gmail.com>
2022-04-05 16:41:07 +0300

    hello UIKit

---
+++ birthday-tracker-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  birthday-tracker-ios
+//
+//  Created by Darvin on 05.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
